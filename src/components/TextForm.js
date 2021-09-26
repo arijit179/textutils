ed8a62c70b9d39018e40e9429b5a55198d7ed7d7
arijit179 <banerjeearijit179@gmail.com>
2021-09-26 13:02:10 +0530

    commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,6 +23,7 @@ export default function TextForm(props) {
         var text = document.getElementById('myBox');
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Text copied to clipboard" , "success");
     }
     const handleExtraSpaces = () => {
