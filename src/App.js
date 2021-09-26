d9f5e07d95cb5284a0da84f9b1e35e041a9614d0
arijit179 <banerjeearijit179@gmail.com>
2021-09-26 08:11:04 +0530

    master

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 function App() {
@@ -47,20 +47,20 @@ function App() {
   }
   return (
     <>
-      {/* <Router> */}
+      <Router>
         <Navbar title="TextUtils" mode={mode} toogleBlue={toogleBlue} />
         <Alert alert={alert} />
         <div className="container my-3" >
-          {/* <Switch> */}
-            {/* <Route exact path="/about"> */}
-              {/* <About mode = {mode} toogleBlue = {toogleBlue}/> */}
-            {/* </Route> */}
-            {/* <Route exact path="/"> */}
+          <Switch>
+            <Route exact path="/about">
+              <About mode = {mode} toogleBlue = {toogleBlue}/>
+            </Route>
+            <Route exact path="/">
               <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-            {/* </Route> */}
-          {/* </Switch> */}
+            </Route>
+          </Switch>
         </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
